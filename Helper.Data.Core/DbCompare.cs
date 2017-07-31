aa1d12b2b85c707ec06dfe93a1f1261eea48c5f8
hc <hongchun_tan1130@hotmail.com>
2017-07-31 14:50:42 +0800

    Added DbCompare.cs for comparing between 2 DbModels

---
+++ Helper.Data.Core/DbCompare.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Helper.Data
+{
+    public class DbCompare
+    {
+        private DbModel source;
+        private DbModel destination;
+        private Dictionary<string, string> DictMatchFields { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private Dictionary<string, Func<object, object>> DictMapFields = new Dictionary<string, Func<object, object>>();
+
+        public DbCompare(DbModel sourceModel, DbModel destinationModel, Dictionary<string, string> dictMatchFields = null)
+        {
+            this.source = sourceModel;
+            this.destination = destinationModel;
+            this.DictMatchFields = dictMatchFields;
+        }
+
+        public string MapField(string fieldName)
+        {
+            if (DictMatchFields.TryGetValue(fieldName, out string val))
+            {
+                return val;
+            }
+            else
+            {
+                return fieldName;
+            }
+        }
+
+        public DbCompare MapValue(string key, Func<object, object> value)
+        {
+            DictMapFields.Add(key, value);
+
+            return this;
+        }
+
+        public Tuple<DbModel, DbModel> CompareDbModels()
+        {
+            var diffSource = new DbModel();
+            var diffDestination = new DbModel();
+
+            Tuple<DbModel, DbModel> compareResult = new Tuple<DbModel, DbModel>(diffSource, diffDestination);
+
+            //left to right compare
+            var sourceKeys = source.Data.Keys.Except(destination.Data.Keys);    //source has desti no
+            foreach (var sourceKey in sourceKeys)
+            {
+                var sourceData = source[sourceKey];
+
+                if (DictMapFields.TryGetValue(sourceKey, out Func<object, object> Map))
+                {
+                    diffSource[MapField(sourceKey)] = Map(sourceData);
+                    diffDestination[MapField(sourceKey)] = null;    //destinationData = null
+                }
+                else
+                {
+                    diffSource[MapField(sourceKey)] = sourceData;
+                    diffDestination[MapField(sourceKey)] = null;
+                }
+            }
+
+            //right to left compare
+            var xDestinationKeys = destination.Data.Keys.Except(source.Data.Keys, StringComparer.OrdinalIgnoreCase);  //desti has source no
+            foreach (var xDestinationKey in xDestinationKeys)
+            {
+                var destinationData = destination[xDestinationKey];
+
+                if (DictMapFields.TryGetValue(xDestinationKey, out Func<object, object> Map))
+                {
+                    diffDestination[MapField(xDestinationKey)] = Map(destinationData);
+                    diffSource[MapField(xDestinationKey)] = null;    //destinationData = null
+                }
+                else
+                {
+                    diffDestination[MapField(xDestinationKey)] = destinationData;
+                    diffSource[MapField(xDestinationKey)] = null;
+                }
+            }
+
+            //cross compare for same fields
+            var intersectKeys = source.Data.Keys.Intersect(destination.Data.Keys);
+            foreach (var intersectKey in intersectKeys)
+            {
+                var sourceData = source[intersectKey];
+                var destinationData = destination[intersectKey];
+                if (sourceData is IComparable)
+                {
+                    if (((IComparable)sourceData).CompareTo(destinationData) != 0)    //different value, need to record
+                    {
+                        if (DictMapFields.TryGetValue(intersectKey, out Func<object, object> Map))
+                        {
+                            diffSource[MapField(intersectKey)] = Map(sourceData);
+                            diffDestination[MapField(intersectKey)] = Map(destinationData);
+                        }
+                        else
+                        {
+                            diffSource[MapField(intersectKey)] = sourceData;
+                            diffDestination[MapField(intersectKey)] = destinationData;
+                        }
+                    }
+                }
+                else if (!sourceData.Equals(destinationData)) //different value, need to record
+                {
+                    if (DictMapFields.TryGetValue(intersectKey, out Func<object, object> Map))
+                    {
+                        diffSource[MapField(intersectKey)] = Map(sourceData);
+                        diffDestination[MapField(intersectKey)] = Map(destinationData);
+                    }
+                    else
+                    {
+                        diffSource[MapField(intersectKey)] = sourceData;
+                        diffDestination[MapField(intersectKey)] = destinationData;
+                    }
+                }
+            }
+            #region obsolete
+            //foreach (var sourcePair in source.Data)
+            //{
+            //    if (destination.Data.TryGetValue(sourcePair.Key, out object destinationData))  //same key/field
+            //    {
+            //        if (sourcePair.Value is IComparable)
+            //        {
+            //            if (((IComparable)sourcePair.Value).CompareTo(destinationData) != 0)    //different value
+            //            {
+            //                diffSource[sourcePair.Key] = sourcePair.Value;
+            //                diffDestination[sourcePair.Key] = destinationData;
+
+            //                containKeys.Add(sourcePair.Key);
+            //            }
+            //        }
+            //        else if (!sourcePair.Value.Equals(destinationData)) //different value
+            //        {
+            //            diffSource[sourcePair.Key] = sourcePair.Value;
+            //            diffDestination[sourcePair.Key] = destinationData;
+
+            //            containKeys.Add(sourcePair.Key);
+            //        }
+            //    }
+            //    else
+            //    {   //source has a key but destination no such key
+            //        diffSource[sourcePair.Key] = sourcePair.Value;
+            //        diffDestination[sourcePair.Key] = destinationData;   //destinationData = null
+
+            //        containKeys.Add(sourcePair.Key);
+            //    }
+            //}
+
+            //foreach (var destinationPair in destination.Data)
+            //{
+            //    if (!containKeys.Contains(destinationPair.Key))
+            //    {
+            //        if (source.Data.TryGetValue(destinationPair.Key, out object sourceData))  //same key/field
+            //        {
+            //            if (destinationPair.Value is IComparable)
+            //            {
+            //                if (((IComparable)destinationPair.Value).CompareTo(sourceData) != 0)    //different value
+            //                {
+            //                    diffDestination[destinationPair.Key] = destinationPair.Value;
+            //                    diffSource[destinationPair.Key] = sourceData;
+
+            //                    containKeys.Add(destinationPair.Key);
+            //                }
+            //            }
+            //            else if (!destinationPair.Value.Equals(sourceData)) //different value
+            //            {
+            //                diffDestination[destinationPair.Key] = destinationPair.Value;
+            //                diffSource[destinationPair.Key] = sourceData;
+
+            //                containKeys.Add(destinationPair.Key);
+            //            }
+            //        }
+            //        else
+            //        {   //destination has a key but source no such key
+            //            diffDestination[destinationPair.Key] = destinationPair.Value;
+            //            diffSource[destinationPair.Key] = sourceData;   //sourceData = null
+
+            //            containKeys.Add(destinationPair.Key);
+            //        }
+            //    }
+            //}
+            #endregion
+            return compareResult;
+        }
+    }
+}
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -2,6 +2,7 @@
 using Helper.Data;
 using MySql.Data.MySqlClient;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Tester
 {
@@ -9,18 +10,51 @@ namespace Tester
     {
         static void Main(string[] args)
         {
-            var t = new Task(async () =>
+            DbModel original = new DbModel();
+            original["deviceID"] = "96837524";
+            original["description"] = "Car Tracker 8 Fault";
+            original["equipmenttype"] = "Pulse GPRS 3";
+            original["revereseGeocode"] = 0;
+            original["imeiNumber"] = "352544071897582";
+            original["isActive"] = 1;
+            original["FK_accountID"] = 1;
+           
+
+            DbModel bnew = new DbModel();
+            bnew["deviceID"] = "96837524";
+            bnew["description"] = "Car Tracker 8 Fault";
+            bnew["equipmenttype"] = "Pulse GPRS 2";
+            bnew["revereseGeocode"] = 0;
+            bnew["imeiNumber"] = "352544071897582";
+            bnew["isActive"] = 0;
+            bnew["nf"] = 1;
+            bnew["FK_accountID"] = 2;
+            bnew["FK_profileID"] = 2;
+
+            Dictionary<string, string> dictField = new Dictionary<string, string>()
             {
-                using (var db = new DbHelper(new MySqlConnection("server=localhost;port=13306;user id=dev;Password=a993cH0lT3cH;database=webtrack2;persist security info=False;Pooling=true;UseCompression=true")))
+                {"FK_accountID", "AccountName"},
+                {"FK_profileID", "ProfileName"},
+            };
+
+            var diff = new DbCompare(original, bnew, dictField);
+            using (var db = new DbHelper(new MySqlConnection("server=localhost;port=13306;user id=root;Password=echol123tech;database=certisqr;persist security info=False;Pooling=true;UseCompression=true")))
+            {
+                diff.MapValue("FK_accountID", (val) =>
                 {
-                    for (int i = 0; i < 100000; i++)
-                    {
-                        var d = await db.Query("SELECT * FROM user WHERE ID = {0}", 1).ToJObject();
-                    }
-                }
-            });
-            t.Start();
-            t.Wait();
+                    return db.Query("SELECT accountName FROM account WHERE id = {0}", val).Value<string>().Result;
+                }).MapValue("FK_profileID", (val) =>
+                {
+                    return db.Query("SELECT name FROM profile WHERE id = {0}", val).Value<string>().Result;
+                }).MapValue("isActive", (val) =>
+                {
+                    return ((int)val == 1) ? "Active" : "Inactive";
+                });
+   
+            
+                var abc = diff.CompareDbModels();
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
